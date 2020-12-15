bd9613483b528b0753aeadc978f4c4e1d9f293e8
jepen <1713074606>
2020-12-15 20:30:06 +0800

    完成作业系统前端的编

--- src/main/java/com/whut/equipmanage/controller/UserController.java
+++ src/main/java/com/whut/equipmanage/controller/UserController.java
@@ -1,12 +1,15 @@
 package com.whut.equipmanage.controller;
 
 import com.whut.equipmanage.common.ResponseBean;
+import com.whut.equipmanage.dataobject.WorkerDO;
 import com.whut.equipmanage.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -16,17 +19,17 @@ public class UserController {
     @Autowired
     UserService userService;
 
-//    @RequestMapping("/users")
-//    public ResponseBean userList(@RequestParam("page") int page,
-//                                 @RequestParam("limit") int limit){
-//
-//        List<UserDO> userDOS = userService.selectByPage(page, limit);
-//        ResponseBean responseBean = new ResponseBean();
-//        responseBean.setData(userDOS);
-//        responseBean.setCode(0);
-//        responseBean.setCount(userDOMapper.countUser());
-//        responseBean.setMsg("ok");
-//        return responseBean;
-//    }
+    @RequestMapping("/workers")
+    public ResponseBean userList(){
+        ResponseBean responseBean = new ResponseBean();
+        List<WorkerDO> list = new ArrayList<>();
+        WorkerDO workerDO = new WorkerDO();
+        workerDO.setWorkerId(1);
+        workerDO.setWorkerName("张三");
+
+        list.add(workerDO);
+        responseBean.setData(list);
+        return  responseBean;
+    }
 
 }
